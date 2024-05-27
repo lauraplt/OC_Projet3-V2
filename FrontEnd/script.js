1b306f26779f094bc0d542be4e1d6497d525a03b
Laura Polet <laurapplt@gmail.com>
2024-05-27 10:04:11 +0200

    Refactor the code

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,133 +1,116 @@
 "use strict";
 
-const url = "http://localhost:5678/api/works";
+const apiUrl = "http://localhost:5678/api";
+const worksEndpoint = "/works";
+const loginEndpoint = "/login";
 
-const divFilters = document.getElementById("filters");
-const divProjects = document.getElementById("gallery");
+const filtersElement = document.getElementById("filters");
+const projectsElement = document.getElementById("gallery");
+const loginForm = document.getElementById("loginForm");
+const errorMessage = document.getElementById("error-message");
 
-// Request from API
-fetch(url)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Erreur de la requête HTTP");
-    }
-    return response.json();
-  })
-  .then((works) => {
-    // Generate filters
-    const portfolioSection = new Set();
-    works.forEach((project) => {
-      portfolioSection.add(project.category.name);
-    });
-
-    const tableProjects = Array.from(portfolioSection);
-    let filterHTML =
-      '<button id="Tous" class="button-filter selected">Tous</button>';
-    tableProjects.forEach((filter) => {
-      const filterId = filter.replace(/\s+/g, "-"); // Ensure valid HTML IDs
-      filterHTML += `<button id="${filterId}" class="button-filter">${filter}</button>`;
-    });
-
-    divFilters.innerHTML = filterHTML;
-
-    // Add event listeners for filters
-    document.querySelectorAll(".button-filter").forEach((button) => {
-      button.addEventListener("click", () => {
-        displayWorks(works, button.innerText);
-
-        // Remove 'selected' class of every buttons
-        document.querySelectorAll(".button-filter").forEach((btn) => {
-          btn.classList.remove("selected");
-        });
-
-        // Add the class 'selected' for the selected button
-        button.classList.add("selected");
-      });
-    });
-
-    // Display all works initially
-    displayWorks(works, "Tous");
-  })
-  .catch((error) => {
-    console.error("Erreur:", error);
-  });
-
-// Function to display works based on filter
-function displayWorks(works, filter) {
-  divProjects.innerHTML = ""; // Clear existing images
-
-  const filteredWorks =
-    filter === "Tous"
-      ? works
-      : works.filter((work) => work.category.name === filter);
-
-  // Loop on filtered works and create DOM work element
-  filteredWorks.forEach((work) => createWork(work));
-}
-
-/**
- * Create a DOM Element for a work
- *
- * @param {Object} work
- */
-function createWork(work) {
-  let el_img = document.createElement("img");
-  el_img.src = work.imageUrl;
-  el_img.alt = work.title;
-
-  let el_title = document.createElement("p");
-  el_title.textContent = work.title;
-
-  let el_item = document.createElement("div");
-  el_item.classList.add("work-item");
-  el_item.append(el_img);
-  el_item.append(el_title);
-
-  divProjects.append(el_item);
-}
-
-// Login Form Handling
-document.addEventListener('DOMContentLoaded', () => {
-  const loginForm = document.getElementById('loginForm');
-
-  loginForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const username = document.getElementById('username').value; 
-    const password = document.getElementById('password').value; 
-
-    fetch('http://localhost:5678/api/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ username, password })
+function fetchData(endpoint) {
+  return fetch(`${apiUrl}${endpoint}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erreur de la requête HTTP");
+      }
+      return response.json();
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Nom d\'utilisateur ou mot de passe incorrect');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log('Login successful:', data);
+    .catch((error) => {
+      console.error("Erreur:", error);
+    });
+}
 
-        const token = data.token;
+function getUniqueFilters(works) {
+  return new Set(works.map((project) => project.category.name));
+}
 
-        if (!token) {
-          throw new Error('Token not found in the response');
-        }
-
-        localStorage.setItem('authToken', token);
-        // Redirect to homepage after storing token
-        window.location.href = 'index.html';
-      })
-      .catch((error) => {
-        console.error('Login error:', error);
-
-        const errorMessage = document.getElementById('error-message');
-        errorMessage.textContent = error.message;
-        errorMessage.style.display = 'block';
-      });
+function generateFilterButtons(filters) {
+  let filterHTML = `<button id="Tous" class="button-filter selected">Tous</button>`;
+  filters.forEach((filter) => {
+    const filterId = filter.replace(/\s+/g, "-");
+    filterHTML += `<button id="${filterId}" class="button-filter">${filter}</button>`;
   });
-});
+  filtersElement.innerHTML = filterHTML;
+}
+
+function addFilterEventListeners() {
+  document.querySelectorAll(".button-filter").forEach((button) => {
+    button.addEventListener("click", () => {
+      displayWorks(works, button.innerText);
+      document.querySelectorAll(".button-filter").forEach((btn) => {
+        btn.classList.remove("selected");
+      });
+      button.classList.add("selected");
+    });
+  });
+}
+
+function displayWorks(works, filter) {
+  projectsElement.innerHTML = "";
+  const filteredWorks = filter === "Tous" ? works : works.filter((work) => work.category.name === filter);
+  filteredWorks.forEach(createWork);
+}
+
+function createWork(work) {
+  const workItem = document.createElement("div");
+  workItem.classList.add("work-item");
+
+  const workImage = document.createElement("img");
+  workImage.src = work.imageUrl;
+  workImage.alt = work.title;
+
+  const workTitle = document.createElement("p");
+  workTitle.textContent = work.title;
+
+  workItem.append(workImage, workTitle);
+  projectsElement.append(workItem);
+}
+
+function handleLoginSubmit(event) {
+  event.preventDefault();
+
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  fetch(`${apiUrl}${loginEndpoint}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Nom d'utilisateur ou mot de passe incorrect");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Login successful:", data);
+
+      const token = data.token;
+      if (!token) {
+        throw new Error("Token not found in the response");
+      }
+
+      localStorage.setItem("authToken", token);
+      window.location.href = "index.html";
+    })
+    .catch((error) => {
+      console.error("Login error:", error);
+      errorMessage.textContent = error.message;
+      errorMessage.style.display = "block";
+    });
+}
+
+fetchData(worksEndpoint)
+  .then((works) => {
+    const uniqueFilters = getUniqueFilters(works);
+    generateFilterButtons(uniqueFilters);
+    addFilterEventListeners();
+    displayWorks(works, "Tous");
+  });
+
+loginForm.addEventListener("submit", handleLoginSubmit);
